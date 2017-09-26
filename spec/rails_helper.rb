3edb3ab0b8776dd965632b6c7618e99ab0cc6f99
Sajid Rabbani <sajid@nascenia.com>
2017-09-26 13:23:44 +0600

    Add configuration for database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,10 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
