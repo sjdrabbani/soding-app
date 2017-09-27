58c301c6802fc3a2047e98b1583ed6760f39d4c9
Sajid Rabbani <sajid@nascenia.com>
2017-09-27 09:40:24 +0600

    Implement task creation feature
    - Add link of tasks list in dashboard page
    - Modify messages in the test of creating a task

--- spec/features/tasks/create_task_spec.rb
+++ spec/features/tasks/create_task_spec.rb
@@ -9,14 +9,14 @@ RSpec.feature 'User create a task' do
   scenario 'with valid input' do
     visit '/'
 
-    click_link 'Task list'
-    click_link 'New task'
+    click_link 'Task List'
+    click_link 'New Task'
     
     fill_in 'Name', with: 'First Task'
     fill_in 'Description', with: 'This is a test description of first task'
-    click_button 'Create task'
+    click_button 'Create Task'
 
-    expect(page).to have_content('Task has been created')
+    expect(page).to have_content('Task was successfully created')
 
     @task = Task.last
     expect(current_path).to eq(task_path(@task))
