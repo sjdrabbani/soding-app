d717c47528f84023aaa82e38bc8b34a4f2ab1cd4
Sajid Rabbani <sajid@nascenia.com>
2017-09-27 09:23:43 +0600

    Modify routing for task

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  resources :tasks
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   root to: 'dashboard#index'
-
-  resources :users do
-    resources :tasks
-  end
 end
